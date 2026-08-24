3f5b9529b9816b1585124a16990626650d6d6056
LingTian66z <changle666@foxmail.com>
2026-08-24 19:18:37 +0800

    第一个物品，屎

---
+++ src/main/java/net/Lngltnat/OilSalt/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.Lngltnat.OilSalt.item;
+
+import net.Lngltnat.OilSalt.OilSalt;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(OilSalt.MODID);
+
+    public static final DeferredItem<Item> SHIT = ITEMS.register("shit",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
